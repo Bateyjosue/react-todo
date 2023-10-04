565c0f08841d57d37d9b27db763e0be5bb9542dd
Bateyjosue <josuebatey19@gmail.com>
2023-10-04 09:05:39 +0200

    add input field for todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ function App() {
 
   return (
     <>
-      <h1 className="text-7xl font-bold text-center">My todo List</h1>
+      <main className="container mx-auto px-8">
+        <h1 className="text-8xl font-bold text-center my-10 opacity-30">todos</h1>
+        <div className="border flex justify-between rounded-full p-2">
+          <input 
+            type="text" 
+            name="todo"
+            placeholder="Add todo..."
+            />
+            <span className="material-symbols-outlined">add_circle</span>
+        </div>
+      </main>
     </>
   )
 }
